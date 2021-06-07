57b8a2047b50651f6a4a78be78d694efa1644e2e
Akarshana1234567 <71764535+Akarshana1234567@users.noreply.github.com>
2021-06-07 17:01:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function Manymo(){
+    window.location="clone.html";
+}
+function Smallscript(){
+    window.location="bon.html";
+}
+function Eni(){
+    window.location="cosocl.html";
+}
+function Sam(){
+    window.location="toys.html";
+}
